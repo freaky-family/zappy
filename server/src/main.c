f221e88333cf95f791130407369e4ee4ba0a6969
Hugo ARNAL <hugo@hugoarnal.com>
2026-06-01 11:37:50 +0200

    feat: simple arguments handling

--- server/src/main.c
+++ server/src/main.c
@@ -1,7 +1,15 @@
+#include "args.h"
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
-    return 84;
+    args_t *args = parse_args(argc, argv);
+
+    if (args == NULL || args->valid == false) {
+        args_free(args);
+        return 84;
+    }
+    printf("Valid arguments!\n");
+    args_free(args);
+    return 0;
 }
