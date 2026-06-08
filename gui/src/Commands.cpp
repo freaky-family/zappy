9064889e635e39d62c8b5b695430281656970e2b
Esteban Hazanas <esteban.hazanas@epitech.eu>
2026-06-08 17:07:58 +0200

    feat: Communication wip

---
+++ gui/src/Commands.cpp
@@ -0,0 +1,74 @@
+#include "Communication.hpp"
+
+void zappy::Communication::msz(std::vector<std::string> params)
+{}
+
+void zappy::Communication::bct(std::vector<std::string> params)
+{}
+
+void zappy::Communication::tna(std::vector<std::string> params)
+{}
+
+void zappy::Communication::pnw(std::vector<std::string> params)
+{}
+
+void zappy::Communication::ppo(std::vector<std::string> params)
+{}
+
+void zappy::Communication::plv(std::vector<std::string> params)
+{}
+
+void zappy::Communication::pin(std::vector<std::string> params)
+{}
+
+void zappy::Communication::pex(std::vector<std::string> params)
+{}
+
+void zappy::Communication::pbc(std::vector<std::string> params)
+{}
+
+void zappy::Communication::pic(std::vector<std::string> params)
+{}
+
+void zappy::Communication::pie(std::vector<std::string> params)
+{}
+
+void zappy::Communication::pfk(std::vector<std::string> params)
+{}
+
+void zappy::Communication::pdr(std::vector<std::string> params)
+{}
+
+void zappy::Communication::pgt(std::vector<std::string> params)
+{}
+
+void zappy::Communication::pdi(std::vector<std::string> params)
+{}
+
+void zappy::Communication::enw(std::vector<std::string> params)
+{}
+
+void zappy::Communication::ebo(std::vector<std::string> params)
+{}
+
+void zappy::Communication::edi(std::vector<std::string> params)
+{}
+
+void zappy::Communication::sgt(std::vector<std::string> params)
+{}
+
+void zappy::Communication::sst(std::vector<std::string> params)
+{}
+
+void zappy::Communication::seg(std::vector<std::string> params)
+{}
+
+void zappy::Communication::smg(std::vector<std::string> params)
+{}
+
+void zappy::Communication::suc(std::vector<std::string> params)
+{}
+
+void zappy::Communication::sbp(std::vector<std::string> params)
+{}
+
