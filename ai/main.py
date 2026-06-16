236bdc28ac2c7cb8432eb3f728c926135fda562b
Nadal <nadal.berthelon@epitech.eu>
2026-06-16 17:25:38 +0200

    [FIX: AI] -h instead of -H for host w/ argparse

--- ai/main.py
+++ ai/main.py
@@ -3,10 +3,11 @@ from argparse import ArgumentParser
 
 
 def parsing():
-    parsing = ArgumentParser()
+    parsing = ArgumentParser(add_help=False)
     parsing.add_argument("-p", "--port", type=int, help="port number", required=True)
     parsing.add_argument("-n", "--name", type=str, help="Team name", required=True)
-    parsing.add_argument("-H", "--host", type=str, help="Host name of the machine", default="127.0.0.1", required=False)
+    parsing.add_argument("-h", "--host", type=str, help="Host name of the machine", default="127.0.0.1", required=False)
+    parsing.add_argument('--help', action='help', help='show this help message and exit')
     return parsing.parse_args()
 
 def main():
