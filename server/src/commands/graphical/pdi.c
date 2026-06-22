75c09768ffa28550f39b25c531b925820151696f
Bastien Sukiennik <bastien.sukiennik@epitech.eu>
2026-06-22 16:45:36 +0200

    [ADD] server to gui command #n refacto & death of a player for starving command

--- server/src/commands/graphical/pdi.c
+++ server/src/commands/graphical/pdi.c
@@ -1,8 +1,10 @@
+#include "players.h"
 #include "server.h"
 #include "messages.h"
 #include <stdio.h>
 
 void command_graphic_pdi_index(server_t *server, int graphic_i, int player_i)
 {
-    dprintf(*CLIENT_I(graphic_i)->fd, "pdi #%d" ZMSG_END_SEQ, player_i);
+    printf("%d\n", PLAYER_I(player_i)->player_graphical_index);
+    dprintf(*CLIENT_I(graphic_i)->fd, "pdi #%d" ZMSG_END_SEQ, PLAYER_I(player_i)->player_graphical_index);
 }
