baa4b9acc85d89f2ccf51d32620315d62930aeed
sacha <sachadef31@gmail.com>
2026-06-29 11:36:31 +0200

    [ADD] more shaders and constexpr values for initialisaion of models and shaders

--- gui/include/ShaderHolder.hpp
+++ gui/include/ShaderHolder.hpp
@@ -4,6 +4,11 @@
 #include <optional>
 
 namespace zappy {
+    constexpr std::string_view PIXELIZER = "assets/shaders/pixelizer.fs";
+    constexpr std::string_view SOBEL = "assets/shaders/sobel.fs";
+    constexpr std::string_view FISHEYE = "assets/shaders/fisheye.fs";
+    constexpr std::string_view POSTERIZATION = "assets/shaders/posterization.fs";
+    constexpr std::string_view BLOOM = "assets/shaders/bloom.fs";
     class ShaderHolder {
         public:
             ShaderHolder();
